af32a6d07459ab6dd4b31e2a5e9e5a5d9cde98df
yhabbeh <yousef.habbeh@mail.ru>
2022-04-20 15:51:52 +0300

    20-1/4/2022

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,8 +1,7 @@
-import 'package:dartz/dartz.dart';
+ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:test1/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:test1/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
 import '../../../../core/error/failures.dart';
 import '../../../../core/error/usecases/usecases.dart';
 
