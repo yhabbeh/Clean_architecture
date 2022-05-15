016a7a0825ce09ce02b1a48fe73240f80dc81e28
YOUSEF <yousef.habbeh@mail.ru>
2022-05-15 07:21:25 +0300

    28/4/2022

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,19 +1,60 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../injection_container.dart';
+import '../bloc/number_trivia_bloc.dart';
 
 class NumberTriviaPage extends StatelessWidget {
-  const NumberTriviaPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Number Trivia"),
+        title: Text('Number Trivia'),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Hello Wolrd"),
+      body: buildBody(context),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc>buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              // Top half
+              Container(
+                // Third of the size of the screen
+                height: MediaQuery.of(context).size.height / 3,
+                // Message Text widgets / CircularLoadingIndicator
+                child: Placeholder(),
+              ),
+              SizedBox(height: 20),
+              // Bottom half
+              Column(
+                children: <Widget>[
+                  // TextField
+                  Placeholder(fallbackHeight: 40),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        // Search concrete button
+                        child: Placeholder(fallbackHeight: 30),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        // Random button
+                        child: Placeholder(fallbackHeight: 30),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Number Trivia',
-      home: const NumberTriviaPage(),
+      home: NumberTriviaPage(),
     );
   }
 }
